4d27428a585577bf953dcbc92af7910f8f4faef9
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2021-10-22 21:06:54 +0200

    added constructors

--- include/mpc_local_planner/types.h
+++ include/mpc_local_planner/types.h
@@ -5,6 +5,14 @@
 
 namespace mpc {
     struct State {
+        State() = default;
+
+        State(double x, double y, double psi, double vel, double cte, double epsi) :
+            x{x}, y{y}, psi{psi}, vel{vel}, cte{cte}, epsi{epsi}
+        {
+
+        }
+
         double x;
         double y;
         double psi;
@@ -14,16 +22,49 @@ namespace mpc {
     };
 
     struct Input {
+        Input() = default;
+
+        Input(double a, double delta) :
+            a{a}, delta{delta}
+        {
+
+        }
+
         double a;
         double delta;
     };
 
     struct OptVariables {
+        OptVariables() = default;
+
+        OptVariables(const State& x, const Input& u) :
+            x{x}, u{u}
+        {
+
+        }
+
         State x;
         Input u;
     };
 
     struct MPCReturn {
+        MPCReturn() = default;
+
+        MPCReturn(const Input& u0, const std::vector<OptVariables>& mpcHorizon,
+                double computeTime, double cost, bool success) :
+                u0{u0}, mpcHorizon{mpcHorizon}, computeTime{computeTime},
+                cost{cost}, success{success}
+        {
+
+        }
+
+        MPCReturn(const std::vector<OptVariables>& mpcHorizon,
+                double computeTime, double cost, bool success) :
+                MPCReturn{mpcHorizon.at(0).u, mpcHorizon, computeTime, cost, success}
+        {
+            
+        }
+
         Input u0;
         std::vector<OptVariables> mpcHorizon;
         double computeTime;
