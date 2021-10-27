647033e74c967619fcbaa03b8416072a939f07d5
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2021-10-27 20:06:00 +0200

    tesing of mpc, problems with cost = 0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
-#include "mpc_local_planner/CppADMPC.h"
 #include "mpc_local_planner/utilities.h"
-#include "mpc_local_planner/TrajectoryTracking/TrajectoryMPC.h"
+#include "mpc_local_planner/MPC.h"
 
 #include <iostream>
 #include <thread>
@@ -8,34 +7,59 @@
 
 #include <ros/ros.h>
 
+nav_msgs::Path getPathFromPoly(const Eigen::Vector4d& coeffs){
+    double start = -30, finish = 30;
+    double step = 0.1;
+    nav_msgs::Path path;
+    std_msgs::Header header;
+    header.frame_id = "odom";
+    header.stamp = ros::Time::now();
+    path.header = header;
+    header.frame_id = "base_link";
+    for(double x = start; x < finish; x += step){
+        double y = coeffs[0] + coeffs[1] * x + coeffs[2] * x * x + coeffs[3] * x * x * x;
+        geometry_msgs::PoseStamped pose;
+        pose.pose.position.x = x;
+        pose.pose.position.y = y;
+        pose.header = header;
+        path.poses.push_back(pose);
+    }
+    return path;
+}
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "MPC");
     ROS_INFO("MPC node intitialized");
     ros::NodeHandle nh;
+    ros::Publisher mpcPathPub;
+    mpcPathPub = nh.advertise<nav_msgs::Path>("local_path", 1);
     ros::Publisher pathPub;
-    pathPub = nh.advertise<nav_msgs::Path>("local_path", 1);
-    // mpc::State refState{10, 10, M_PI};
+    pathPub = nh.advertise<nav_msgs::Path>("global_path", 1);
     Eigen::Vector4d coeffs;
-    coeffs << 0, 0, 0, -0.01;
-    mpc::State state{0, 2, M_PI, 0};
-    mpc::nonlinear::TrajectoryTracking::MPC mpc{coeffs};
+    coeffs << 0, 1, 0.1, -0.01;
+    auto globalPath = getPathFromPoly(coeffs);
+    mpc::State state{-15, 10, 0, 0, 0, 0};
+    mpc::MPC MPC{coeffs};
     std::cout << "x\ty\tpsi\tvel\tcost\n";
     auto refresh = std::chrono::high_resolution_clock::now();
     while(true){
-        auto fullState = mpc.calcState(state);
-        // std::cout << fullState[4] << '\t' << fullState[5] << '\n';
-        auto solution = mpc.solve(fullState);
-        state = solution.mpcHorizon[1].x;
+        MPC.calcState(state);
+        auto solution = MPC.solve(state);
+        mpc::logSolution(solution, state, "log.txt");
+        if (solution.cost == 0) {
+            mpc::logSolution(solution, state, "logZeroCost.txt");
+        }
+        mpc::model(state, solution.u0);
         std::cout << state.x << '\t' << state.y << '\t' << state.psi 
             << '\t' << state.vel << '\t' << solution.cost << std::endl;
-        // std::cout << "Time: " << solution.computeTime << " [ms]\n";
-        pathPub.publish(getPathMsg(solution));
-        if (solution.cost < 100){
-            state = mpc::State{0, 0, 0, 0};
+        std::cout << "Time: " << solution.computeTime << " [ms]\n";
+        mpcPathPub.publish(getPathMsg(solution));
+        if (solution.cost < 100 && solution.cost > 0.1){
+            state = mpc::State{0, 2, 0, 0, 0, 0};
         }
+        pathPub.publish(globalPath);
         refresh += std::chrono::milliseconds{100};
         std::this_thread::sleep_until(refresh);
     }
-
     return 0;
 }
