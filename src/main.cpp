dc6875a95650becc5e3e4f46eecb112a0dafb4d3
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2021-11-11 17:17:59 +0100

    log to mpc directory

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "MPC");
     ROS_INFO("MPC node intitialized");
     auto track = mpc::getTestTrack();
-    mpc::State initialState{23, 0, M_PI_2, 0, 0, 0};
+    mpc::State initialState{41, 0, M_PI_2, 0, 0, 0};
     constexpr size_t N = 30;
     constexpr double dt = 0.1;
 
@@ -20,16 +20,21 @@ int main(int argc, char** argv){
     std::cout << "x\ty\tpsi\tvel\tcost\n";
     auto refresh = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds{100};
     std::this_thread::sleep_for(std::chrono::seconds(1)); // wait for rviz
+    double totalTime = 0;
+    size_t iterations = 0;
     while(ros::ok()){
         auto solution = MPC.solve(state);
-        mpc::logSolution(solution, state, "log.txt");
+        
+        std::string filePath = __PATH__;
+        filePath += "/logs/log.txt";
+        mpc::logSolution(solution, state, filePath);
         MPC.model(state, solution.u0);
-        // std::cout << state.x << '\t' << state.y << '\t' << state.psi 
-        //     << '\t' << state.vel << '\t' << solution.cost << std::endl;
+
         std::cout << "Time: " << solution.computeTime << " [ms]\n";
-        if (solution.cost < 100 && solution.cost > 0.1){
-            state = initialState;
-        }
+        totalTime += solution.computeTime;
+        iterations++;
+        std::cout << "Avg. time: " << totalTime / iterations << " [ms]\n";
+
         std::this_thread::sleep_until(refresh);
         refresh = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds{(int)(1000 * dt)};
     }
