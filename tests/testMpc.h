1935eeadaba098f7bb1d1e87685f527694523121
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2021-11-21 14:07:45 +0100

    sending in steering angle

--- tests/testMpc.h
+++ tests/testMpc.h
@@ -8,29 +8,29 @@
 namespace mpc {
     void testMpc() {
         auto track = mpc::getTestTrack();
-        mpc::State initialState{41, 0, M_PI_2, 0, 0, 0};
+        mpc::OptVariables initialOptVars{State{81, 0, M_PI_2, 5.0, 0, 0}, Input{0, 0}};
         constexpr size_t N = 30;
         constexpr double dt = 0.1;
 
-        mpc::State state = initialState;
+        mpc::OptVariables optVars = initialOptVars;
         mpc::MPC MPC{track, N, dt};
-        std::cout << "x\ty\tpsi\tvel\tcost\n";
         auto refresh = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds{100};
         std::this_thread::sleep_for(std::chrono::seconds(1)); // wait for rviz
         double totalTime = 0;
         size_t iterations = 0;
         while(ros::ok()){
-            auto solution = MPC.solve(state);
+            auto solution = MPC.solve(optVars);
             
             std::string filePath = __PATH__;
             filePath += "/logs/log.txt";
-            mpc::logSolution(solution, state, filePath);
-            MPC.model(state, solution.u0);
+            mpc::logSolution(solution, optVars.x, filePath);
+            MPC.model(optVars, solution.u0);
 
             std::cout << "Time: " << solution.computeTime << " [ms]\n";
             totalTime += solution.computeTime;
             iterations++;
-            std::cout << "Avg. time: " << totalTime / iterations << " [ms]\n";
+            std::cout << "Avg. time: " << static_cast<int>(totalTime / iterations) << " [ms]\n";
+            std::cout << "Vel: " << optVars.x.vel << std::endl;
 
             std::this_thread::sleep_until(refresh);
             refresh = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds{(int)(1000 * dt)};
