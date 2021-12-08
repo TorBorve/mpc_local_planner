1e41b72cdd31943bfcf27bf8ea5fc0acd918c53f
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2021-12-08 15:39:51 +0100

    constant for loop dt

--- include/mpc_local_planner/constants.h
+++ include/mpc_local_planner/constants.h
@@ -6,6 +6,10 @@
 
 #define MPC_dt 0.2 // time increment in between steps
 
+#define MPC_LOOP_Hz 30 // the hertz of the mpc. It could hurt performance if this is too low
+
+#define MPC_LOOP_dt 1.0 / MPC_LOOP_Hz // time between outputs from the MPC
+
 // Constants for car
 
 #define MPC_MAX_STEERING_ANGLE 0.57 // maxium steering angle on the wheels in radians
@@ -14,4 +18,4 @@
 
 #define MPC_MAX_STEERING_ROTATION_SPEED 800.0 / 17.3 * M_PI / 180.0 // maximum ratation speed on the wheels in [rad / s]
 
-#define MPC_WHEELBASE 2.65
+#define MPC_WHEELBASE 2.65 // the length between the front and rear axle
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "mpc_local_planner/RosMpc.h"
+#include "mpc_local_planner/constants.h"
 #include <ros/ros.h>
 
 int main(int argc, char** argv){
@@ -9,7 +10,7 @@ int main(int argc, char** argv){
     mpc.verifyInputs();
     ROS_INFO("Topics and transfroms are ok!");
     ros::Duration(2).sleep(); // sleep for two sec
-    ros::Rate loopRate(30);
+    ros::Rate loopRate(MPC_LOOP_Hz);
     while(ros::ok()) {
         ros::spinOnce();
         mpc.solve();
