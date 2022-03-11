817178abb5c5f142b89c63b0f43aa8d2ba32fee6
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2022-03-11 21:31:54 +0100

    formatting google style

--- src/AcadosSolver.cpp
+++ src/AcadosSolver.cpp
@@ -1,193 +1,177 @@
 #include "mpc_local_planner/AcadosSolver.h"
+
 #include <ros/ros.h>
 
-namespace mpc
-{
-    AcadosSolver::AcadosSolver(const OptVariables &optVars)
-    {
-        init();
-        setInitGuess(optVars);
-        return;
-    }
+namespace mpc {
 
-    AcadosSolver::~AcadosSolver()
-    {
-        freeAllocated();
-        return;
-    }
+AcadosSolver::AcadosSolver(const OptVariables &optVars) {
+    init();
+    setInitGuess(optVars);
+    return;
+}
 
-    void AcadosSolver::reInit(const OptVariables &optVars)
-    {
-        freeAllocated();
-        init();
-        setInitGuess(optVars);
-        return;
-    }
+AcadosSolver::~AcadosSolver() {
+    freeAllocated();
+    return;
+}
 
-    void AcadosSolver::setInitCondition(const OptVariables &optVars)
-    {
-        // initial condition
-        int idxbx0[NBX0];
-        idxbx0[0] = 0;
-        idxbx0[1] = 1;
-        idxbx0[2] = 2;
-        idxbx0[3] = 3;
-        idxbx0[4] = 4;
-        idxbx0[5] = 5;
+void AcadosSolver::reInit(const OptVariables &optVars) {
+    freeAllocated();
+    init();
+    setInitGuess(optVars);
+    return;
+}
 
-        double lbx0[NBX0];
-        double ubx0[NBX0];
-        lbx0[0] = optVars.x.x;
-        ubx0[0] = optVars.x.x;
-        lbx0[1] = optVars.x.y;
-        ubx0[1] = optVars.x.y;
-        lbx0[2] = optVars.x.psi;
-        ubx0[2] = optVars.x.psi;
-        lbx0[3] = optVars.x.vel;
-        ubx0[3] = optVars.x.vel;
-        lbx0[4] = optVars.u.delta;
-        ubx0[4] = optVars.u.delta;
-        lbx0[5] = optVars.u.throttle;
-        ubx0[5] = optVars.u.throttle;
+void AcadosSolver::setInitCondition(const OptVariables &optVars) {
+    // initial condition
+    int idxbx0[NBX0];
+    idxbx0[0] = 0;
+    idxbx0[1] = 1;
+    idxbx0[2] = 2;
+    idxbx0[3] = 3;
+    idxbx0[4] = 4;
+    idxbx0[5] = 5;
 
-        ocp_nlp_constraints_model_set(config_, dims_, in_, 0, "idxbx", idxbx0);
-        ocp_nlp_constraints_model_set(config_, dims_, in_, 0, "lbx", lbx0);
-        ocp_nlp_constraints_model_set(config_, dims_, in_, 0, "ubx", ubx0);
-    }
+    double lbx0[NBX0];
+    double ubx0[NBX0];
+    lbx0[0] = optVars.x.x;
+    ubx0[0] = optVars.x.x;
+    lbx0[1] = optVars.x.y;
+    ubx0[1] = optVars.x.y;
+    lbx0[2] = optVars.x.psi;
+    ubx0[2] = optVars.x.psi;
+    lbx0[3] = optVars.x.vel;
+    ubx0[3] = optVars.x.vel;
+    lbx0[4] = optVars.u.delta;
+    ubx0[4] = optVars.u.delta;
+    lbx0[5] = optVars.u.throttle;
+    ubx0[5] = optVars.u.throttle;
 
-    void AcadosSolver::setParams(const Eigen::Vector4d &coeffs)
-    {
-        double p[NP];
-        p[0] = coeffs[0];
-        p[1] = coeffs[1];
-        p[2] = coeffs[2];
-        p[3] = coeffs[3];
+    ocp_nlp_constraints_model_set(config_, dims_, in_, 0, "idxbx", idxbx0);
+    ocp_nlp_constraints_model_set(config_, dims_, in_, 0, "lbx", lbx0);
+    ocp_nlp_constraints_model_set(config_, dims_, in_, 0, "ubx", ubx0);
+}
 
-        for (int ii = 0; ii <= dims_->N; ii++)
-        {
-            bicycle_model_acados_update_params(capsule_, ii, p, NP);
-        }
-    }
+void AcadosSolver::setParams(const Eigen::Vector4d &coeffs) {
+    double p[NP];
+    p[0] = coeffs[0];
+    p[1] = coeffs[1];
+    p[2] = coeffs[2];
+    p[3] = coeffs[3];
 
-    MPCReturn AcadosSolver::solve(const OptVariables &optVars, const Eigen::Vector4d &coeffs)
-    {
-        OptVariables optVarsCopy{optVars};
-        static double prevThrottle = 0.0;
-        optVarsCopy.u.throttle = prevThrottle;
-        int N = dims_->N;
-        auto start = std::chrono::high_resolution_clock::now();
-
-        setInitCondition(optVarsCopy);
-        setParams(coeffs);
-
-        // prepare evaluation
-        int NTIMINGS = 1;
-
-        double xtraj[NX * (N + 1)];
-        double utraj[NU * N];
-
-        // solve ocp in loop
-        int rti_phase = 0;
-        int status;
-
-        for (int ii = 0; ii < NTIMINGS; ii++)
-        {
-            ocp_nlp_solver_opts_set(config_, opts_, "rti_phase", &rti_phase);
-            status = bicycle_model_acados_solve(capsule_);
-        }
-
-        // get the solution
-        for (int ii = 0; ii <= dims_->N; ii++)
-            ocp_nlp_out_get(config_, dims_, out_, ii, "x", &xtraj[ii * NX]);
-        for (int ii = 0; ii < dims_->N; ii++)
-            ocp_nlp_out_get(config_, dims_, out_, ii, "u", &utraj[ii * NU]);
-
-        if (status != ACADOS_SUCCESS)
-        {
-            ROS_ERROR("bicycle_model_acados_solve() failed with status %d.\n", status);
-            reInit(optVarsCopy);
-        }
-
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-        MPCReturn ret;
-        ret.mpcHorizon.resize(N);
-        ret.u0 = Input{xtraj[5 + NX], xtraj[4 + NX]};
-        prevThrottle = ret.u0.throttle;
-        for (int i = 0; i < N; i++)
-        {
-            State state{xtraj[0 + NX * i], xtraj[1 + NX * i], xtraj[2 + NX * i], xtraj[3 + NX * i], 0, 0};
-            Input input{xtraj[5 + NX * i], xtraj[4 + NX * i]};
-            ret.mpcHorizon.at(i) = OptVariables{state, input};
-        }
-        ret.cost = -1;
-        ret.success = status == ACADOS_SUCCESS;
-        ret.computeTime = duration.count();
-        return ret;
-    }
-
-    void AcadosSolver::setInitGuess(const OptVariables &optVars)
-    {
-        // initialization for state values
-        double x_init[NX];
-        x_init[0] = optVars.x.x;
-        x_init[1] = optVars.x.y;
-        x_init[2] = optVars.x.psi;
-        x_init[3] = optVars.x.vel;
-        x_init[4] = optVars.u.delta;
-        x_init[5] = optVars.u.throttle;
-
-        // initial value for control input
-        double u0[NU];
-        u0[0] = 0.0;
-        u0[1] = 0.0;
-
-        // initialize solution
-        for (int i = 0; i <= dims_->N; i++)
-        {
-            ocp_nlp_out_set(config_, dims_, out_, i, "x", x_init);
-            ocp_nlp_out_set(config_, dims_, out_, i, "u", u0);
-        }
-    }
-
-    void AcadosSolver::init()
-    {
-        capsule_ = bicycle_model_acados_create_capsule();
-        int N = BICYCLE_MODEL_N;
-        // allocate the array and fill it accordingly
-        double *new_time_steps = NULL;
-        int status = bicycle_model_acados_create_with_discretization(capsule_, N, new_time_steps);
-        config_ = bicycle_model_acados_get_nlp_config(capsule_);
-        dims_ = bicycle_model_acados_get_nlp_dims(capsule_);
-        in_ = bicycle_model_acados_get_nlp_in(capsule_);
-        out_ = bicycle_model_acados_get_nlp_out(capsule_);
-        solver_ = bicycle_model_acados_get_nlp_solver(capsule_);
-        opts_ = bicycle_model_acados_get_nlp_opts(capsule_);
-        if (status)
-        {
-            std::stringstream error;
-            error << "bicycle_model_acados_create() returned status: " << status << ". Exiting.";
-            ROS_ERROR_STREAM(error.str());
-            throw std::runtime_error{error.str()};
-        }
-        return;
-    }
-
-    void AcadosSolver::freeAllocated()
-    {
-        // free solver
-        int status = bicycle_model_acados_free(capsule_);
-        if (status)
-        {
-            ROS_ERROR("bicycle_model_acados_free() returned status %d. \n", status);
-        }
-        // free solver capsule
-        status = bicycle_model_acados_free_capsule(capsule_);
-        if (status)
-        {
-            ROS_ERROR("bicycle_model_acados_free_capsule() returned status %d. \n", status);
-        }
-        return;
+    for (int ii = 0; ii <= dims_->N; ii++) {
+        bicycle_model_acados_update_params(capsule_, ii, p, NP);
     }
 }
+
+MPCReturn AcadosSolver::solve(const OptVariables &optVars, const Eigen::Vector4d &coeffs) {
+    OptVariables optVarsCopy{optVars};
+    static double prevThrottle = 0.0;
+    optVarsCopy.u.throttle = prevThrottle;
+    int N = dims_->N;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    setInitCondition(optVarsCopy);
+    setParams(coeffs);
+
+    // prepare evaluation
+    int NTIMINGS = 1;
+
+    double xtraj[NX * (N + 1)];
+    double utraj[NU * N];
+
+    // solve ocp in loop
+    int rti_phase = 0;
+    int status;
+
+    for (int ii = 0; ii < NTIMINGS; ii++) {
+        ocp_nlp_solver_opts_set(config_, opts_, "rti_phase", &rti_phase);
+        status = bicycle_model_acados_solve(capsule_);
+    }
+
+    // get the solution
+    for (int ii = 0; ii <= dims_->N; ii++)
+        ocp_nlp_out_get(config_, dims_, out_, ii, "x", &xtraj[ii * NX]);
+    for (int ii = 0; ii < dims_->N; ii++)
+        ocp_nlp_out_get(config_, dims_, out_, ii, "u", &utraj[ii * NU]);
+
+    if (status != ACADOS_SUCCESS) {
+        ROS_ERROR("bicycle_model_acados_solve() failed with status %d.\n", status);
+        reInit(optVarsCopy);
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    MPCReturn ret;
+    ret.mpcHorizon.resize(N);
+    ret.u0 = Input{xtraj[5 + NX], xtraj[4 + NX]};
+    prevThrottle = ret.u0.throttle;
+    for (int i = 0; i < N; i++) {
+        State state{xtraj[0 + NX * i], xtraj[1 + NX * i], xtraj[2 + NX * i], xtraj[3 + NX * i], 0, 0};
+        Input input{xtraj[5 + NX * i], xtraj[4 + NX * i]};
+        ret.mpcHorizon.at(i) = OptVariables{state, input};
+    }
+    ret.cost = -1;
+    ret.success = status == ACADOS_SUCCESS;
+    ret.computeTime = duration.count();
+    return ret;
+}
+
+void AcadosSolver::setInitGuess(const OptVariables &optVars) {
+    // initialization for state values
+    double x_init[NX];
+    x_init[0] = optVars.x.x;
+    x_init[1] = optVars.x.y;
+    x_init[2] = optVars.x.psi;
+    x_init[3] = optVars.x.vel;
+    x_init[4] = optVars.u.delta;
+    x_init[5] = optVars.u.throttle;
+
+    // initial value for control input
+    double u0[NU];
+    u0[0] = 0.0;
+    u0[1] = 0.0;
+
+    // initialize solution
+    for (int i = 0; i <= dims_->N; i++) {
+        ocp_nlp_out_set(config_, dims_, out_, i, "x", x_init);
+        ocp_nlp_out_set(config_, dims_, out_, i, "u", u0);
+    }
+}
+
+void AcadosSolver::init() {
+    capsule_ = bicycle_model_acados_create_capsule();
+    int N = BICYCLE_MODEL_N;
+    // allocate the array and fill it accordingly
+    double *new_time_steps = NULL;
+    int status = bicycle_model_acados_create_with_discretization(capsule_, N, new_time_steps);
+    config_ = bicycle_model_acados_get_nlp_config(capsule_);
+    dims_ = bicycle_model_acados_get_nlp_dims(capsule_);
+    in_ = bicycle_model_acados_get_nlp_in(capsule_);
+    out_ = bicycle_model_acados_get_nlp_out(capsule_);
+    solver_ = bicycle_model_acados_get_nlp_solver(capsule_);
+    opts_ = bicycle_model_acados_get_nlp_opts(capsule_);
+    if (status) {
+        std::stringstream error;
+        error << "bicycle_model_acados_create() returned status: " << status << ". Exiting.";
+        ROS_ERROR_STREAM(error.str());
+        throw std::runtime_error{error.str()};
+    }
+    return;
+}
+
+void AcadosSolver::freeAllocated() {
+    // free solver
+    int status = bicycle_model_acados_free(capsule_);
+    if (status) {
+        ROS_ERROR("bicycle_model_acados_free() returned status %d. \n", status);
+    }
+    // free solver capsule
+    status = bicycle_model_acados_free_capsule(capsule_);
+    if (status) {
+        ROS_ERROR("bicycle_model_acados_free_capsule() returned status %d. \n", status);
+    }
+    return;
+}
+}  // namespace mpc
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,219 +1,190 @@
 #include "mpc_local_planner/utilities.h"
 
-#include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
-#include <eigen3/Eigen/Dense>
+#include <tf2/LinearMath/Quaternion.h>
 
+#include <eigen3/Eigen/Dense>
 #include <fstream>
 #include <iomanip>
 
-namespace mpc
-{
-    geometry_msgs::Pose toMsg(const State &state)
-    {
-        geometry_msgs::Pose pose;
-        pose.position.x = state.x;
-        pose.position.y = state.y;
-        tf2::Quaternion quat;
-        quat.setEuler(state.psi, 0, 0);
-        pose.orientation.x = quat.getX();
-        pose.orientation.y = quat.getY();
-        pose.orientation.z = quat.getZ();
-        pose.orientation.w = quat.getW();
-        return pose;
+namespace mpc {
+geometry_msgs::Pose toMsg(const State &state) {
+    geometry_msgs::Pose pose;
+    pose.position.x = state.x;
+    pose.position.y = state.y;
+    tf2::Quaternion quat;
+    quat.setEuler(state.psi, 0, 0);
+    pose.orientation.x = quat.getX();
+    pose.orientation.y = quat.getY();
+    pose.orientation.z = quat.getZ();
+    pose.orientation.w = quat.getW();
+    return pose;
+}
+
+void logSolution(const MPCReturn &solution, const State &curState, const std::string &filename) {
+    std::ofstream outFile(filename);
+    if (!outFile) {
+        std::cout << "could not write to file log.txt\n";
+        return;
     }
-
-
-    void logSolution(const MPCReturn &solution, const State &curState, const std::string &filename)
-    {
-        std::ofstream outFile(filename);
-        if (!outFile)
-        {
-            std::cout << "could not write to file log.txt\n";
-            return;
-        }
-        outFile << "Log for mpc solution\n";
-        outFile << "Cost: " << solution.cost << std::endl;
-        outFile << "Current State:\n";
-        outFile << "x\ty\tpsi\tvel\n";
-        outFile << curState.x << '\t' << curState.y << '\t' << curState.psi << '\t' << curState.vel << '\n';
-        outFile << "State and input from horizon:\n";
-        outFile << "x\ty\tpsi\tvel\tcte\tepsi\ta\tdelta\n";
-        outFile << std::fixed << std::setprecision(5);
-        for (const auto &step : solution.mpcHorizon)
-        {
-            outFile << step.x.x << '\t'
-                    << step.x.y << '\t'
-                    << step.x.psi << '\t'
-                    << step.x.vel << '\t'
-                    << step.x.cte << '\t'
-                    << step.x.epsi << '\t'
-                    << step.u.throttle << '\t'
-                    << step.u.delta << std::endl;
-        }
-    }
-
-    // Fit a polynomial.
-    // Adapted from
-    // https://gist.github.com/ksjgh/4d5050d0e9afc5fdb5908734335138d0
-    Eigen::VectorXd polyfit(const Eigen::VectorXd &xvals, const Eigen::VectorXd &yvals, int order)
-    {
-        assert(xvals.size() == yvals.size());
-        assert(order >= 1 && order <= xvals.size() - 1);
-        Eigen::MatrixXd A(xvals.size(), order + 1);
-
-        for (int i = 0; i < xvals.size(); i++)
-        {
-            A(i, 0) = 1.0;
-        }
-
-        for (int j = 0; j < xvals.size(); j++)
-        {
-            for (int i = 0; i < order; i++)
-            {
-                A(j, i + 1) = A(j, i) * xvals(j);
-            }
-        }
-
-        auto Q = A.householderQr();
-        auto result = Q.solve(yvals);
-        return result;
-    }
-
-    std::vector<Point> getTestTrack()
-    {
-        constexpr size_t n = 400;
-        std::vector<Point> track;
-        // double x_start = -30, x_stop = 30;
-        // for(double x = x_start; x < x_stop; x += (x_stop - x_start) / n) {
-        //     track.push_back(Point{x, 0.5 * x + 0.1 * x * x + -0.01 * x * x * x});
-        // }
-        for (double theta = 0; theta < 2 * M_PI; theta += 2 * M_PI / (double)n)
-        {
-            double radius = 20;
-            track.push_back(Point{2 * radius * cos(theta), radius * sin(1 * theta) + radius / 1.2 * sin(3 * theta)});
-        }
-        return track;
-    }
-
-    nav_msgs::Path getPathMsg(const MPCReturn &solution)
-    {
-        nav_msgs::Path path;
-        std_msgs::Header header;
-        header.frame_id = "odom";
-        header.stamp = ros::Time::now();
-        path.header = header;
-        header.frame_id = "base_link";
-        path.poses.resize(solution.mpcHorizon.size());
-        for (int i = 0; i < path.poses.size(); i++)
-        {
-            path.poses[i].pose = toMsg(solution.mpcHorizon[i].x);
-            path.poses[i].header = header;
-        }
-        return path;
-    }
-
-    nav_msgs::Path getPathMsg(const Eigen::Vector4d &coeffs)
-    {
-        double start = -30, finish = 30;
-        double step = 0.5;
-        nav_msgs::Path path;
-        std_msgs::Header header;
-        header.frame_id = "odom";
-        header.stamp = ros::Time::now();
-        path.header = header;
-        header.frame_id = "base_link";
-        for (double x = start; x < finish; x += step)
-        {
-            double y = coeffs[0] + coeffs[1] * x + coeffs[2] * x * x + coeffs[3] * x * x * x;
-            geometry_msgs::PoseStamped pose;
-            pose.pose.position.x = x;
-            pose.pose.position.y = y;
-            pose.header = header;
-            path.poses.push_back(pose);
-        }
-        return path;
-    }
-
-    nav_msgs::Path getPathMsg(const std::vector<Point> &track)
-    {
-        nav_msgs::Path path;
-        std_msgs::Header header;
-        header.frame_id = "odom";
-        header.stamp = ros::Time::now();
-        path.header = header;
-        header.frame_id = "base_link";
-        for (const auto &p : track)
-        {
-            geometry_msgs::PoseStamped pose;
-            pose.pose.position.x = p.x;
-            pose.pose.position.y = p.y;
-            pose.header = header;
-            path.poses.push_back(pose);
-        }
-        return path;
-    }
-
-    State toState(const nav_msgs::Odometry &odom)
-    {
-        double vel = velocity(odom);
-        double psi = getYaw(odom.pose.pose.orientation);
-        return State{odom.pose.pose.position.x, odom.pose.pose.position.y, psi, vel, 0, 0};
-    }
-
-    double velocity(const nav_msgs::Odometry &odom)
-    {
-        return length(odom.twist.twist.linear);
-    }
-
-    double length(const geometry_msgs::Vector3 &vec)
-    {
-        return sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
-    }
-
-    double getYaw(const geometry_msgs::Quaternion &quat)
-    {
-        tf2::Quaternion q{quat.x, quat.y, quat.z, quat.w};
-        tf2::Matrix3x3 mat{q};
-        double roll, pitch, yaw;
-        mat.getRPY(roll, pitch, yaw);
-        return yaw;
-    }
-
-    bool hasParamError(ros::NodeHandle *nh, const std::string &param)
-    {
-        if (!nh->hasParam(param))
-        {
-            std::string error = "parameter " + param + " is not set. This needs to be specified as a rosparameter in the launchfile";
-            ROS_ERROR_STREAM(error);
-            throw std::runtime_error{error};
-        }
-        return true;
-    }
-
-    bool hasParamWarn(ros::NodeHandle *nh, const std::string &param)
-    {
-        if (!nh->hasParam(param))
-        {
-            std::string error = "parameter " + param + " is not set. This needs to be specified as a rosparameter in the launchfile";
-            ROS_WARN_STREAM(error);
-            return false;
-        }
-        return true;
-    }
-
-    std::vector<Point> toVector(const nav_msgs::Path &path)
-    {
-        std::vector<Point> pathVector(path.poses.size());
-        for (unsigned int i = 0; i < pathVector.size(); i++)
-        {
-            pathVector.at(i) = toPoint(path.poses.at(i).pose.position);
-        }
-        return pathVector;
-    }
-
-    Point toPoint(const geometry_msgs::Point &p)
-    {
-        return Point{p.x, p.y};
+    outFile << "Log for mpc solution\n";
+    outFile << "Cost: " << solution.cost << std::endl;
+    outFile << "Current State:\n";
+    outFile << "x\ty\tpsi\tvel\n";
+    outFile << curState.x << '\t' << curState.y << '\t' << curState.psi << '\t' << curState.vel << '\n';
+    outFile << "State and input from horizon:\n";
+    outFile << "x\ty\tpsi\tvel\tcte\tepsi\ta\tdelta\n";
+    outFile << std::fixed << std::setprecision(5);
+    for (const auto &step : solution.mpcHorizon) {
+        outFile << step.x.x << '\t'
+                << step.x.y << '\t'
+                << step.x.psi << '\t'
+                << step.x.vel << '\t'
+                << step.x.cte << '\t'
+                << step.x.epsi << '\t'
+                << step.u.throttle << '\t'
+                << step.u.delta << std::endl;
     }
 }
+
+// Fit a polynomial.
+// Adapted from
+// https://gist.github.com/ksjgh/4d5050d0e9afc5fdb5908734335138d0
+Eigen::VectorXd polyfit(const Eigen::VectorXd &xvals, const Eigen::VectorXd &yvals, int order) {
+    assert(xvals.size() == yvals.size());
+    assert(order >= 1 && order <= xvals.size() - 1);
+    Eigen::MatrixXd A(xvals.size(), order + 1);
+
+    for (int i = 0; i < xvals.size(); i++) {
+        A(i, 0) = 1.0;
+    }
+
+    for (int j = 0; j < xvals.size(); j++) {
+        for (int i = 0; i < order; i++) {
+            A(j, i + 1) = A(j, i) * xvals(j);
+        }
+    }
+
+    auto Q = A.householderQr();
+    auto result = Q.solve(yvals);
+    return result;
+}
+
+std::vector<Point> getTestTrack() {
+    constexpr size_t n = 400;
+    std::vector<Point> track;
+    // double x_start = -30, x_stop = 30;
+    // for(double x = x_start; x < x_stop; x += (x_stop - x_start) / n) {
+    //     track.push_back(Point{x, 0.5 * x + 0.1 * x * x + -0.01 * x * x * x});
+    // }
+    for (double theta = 0; theta < 2 * M_PI; theta += 2 * M_PI / (double)n) {
+        double radius = 20;
+        track.push_back(Point{2 * radius * cos(theta), radius * sin(1 * theta) + radius / 1.2 * sin(3 * theta)});
+    }
+    return track;
+}
+
+nav_msgs::Path getPathMsg(const MPCReturn &solution) {
+    nav_msgs::Path path;
+    std_msgs::Header header;
+    header.frame_id = "odom";
+    header.stamp = ros::Time::now();
+    path.header = header;
+    header.frame_id = "base_link";
+    path.poses.resize(solution.mpcHorizon.size());
+    for (int i = 0; i < path.poses.size(); i++) {
+        path.poses[i].pose = toMsg(solution.mpcHorizon[i].x);
+        path.poses[i].header = header;
+    }
+    return path;
+}
+
+nav_msgs::Path getPathMsg(const Eigen::Vector4d &coeffs) {
+    double start = -30, finish = 30;
+    double step = 0.5;
+    nav_msgs::Path path;
+    std_msgs::Header header;
+    header.frame_id = "odom";
+    header.stamp = ros::Time::now();
+    path.header = header;
+    header.frame_id = "base_link";
+    for (double x = start; x < finish; x += step) {
+        double y = coeffs[0] + coeffs[1] * x + coeffs[2] * x * x + coeffs[3] * x * x * x;
+        geometry_msgs::PoseStamped pose;
+        pose.pose.position.x = x;
+        pose.pose.position.y = y;
+        pose.header = header;
+        path.poses.push_back(pose);
+    }
+    return path;
+}
+
+nav_msgs::Path getPathMsg(const std::vector<Point> &track) {
+    nav_msgs::Path path;
+    std_msgs::Header header;
+    header.frame_id = "odom";
+    header.stamp = ros::Time::now();
+    path.header = header;
+    header.frame_id = "base_link";
+    for (const auto &p : track) {
+        geometry_msgs::PoseStamped pose;
+        pose.pose.position.x = p.x;
+        pose.pose.position.y = p.y;
+        pose.header = header;
+        path.poses.push_back(pose);
+    }
+    return path;
+}
+
+State toState(const nav_msgs::Odometry &odom) {
+    double vel = velocity(odom);
+    double psi = getYaw(odom.pose.pose.orientation);
+    return State{odom.pose.pose.position.x, odom.pose.pose.position.y, psi, vel, 0, 0};
+}
+
+double velocity(const nav_msgs::Odometry &odom) {
+    return length(odom.twist.twist.linear);
+}
+
+double length(const geometry_msgs::Vector3 &vec) {
+    return sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+double getYaw(const geometry_msgs::Quaternion &quat) {
+    tf2::Quaternion q{quat.x, quat.y, quat.z, quat.w};
+    tf2::Matrix3x3 mat{q};
+    double roll, pitch, yaw;
+    mat.getRPY(roll, pitch, yaw);
+    return yaw;
+}
+
+bool hasParamError(ros::NodeHandle *nh, const std::string &param) {
+    if (!nh->hasParam(param)) {
+        std::string error = "parameter " + param + " is not set. This needs to be specified as a rosparameter in the launchfile";
+        ROS_ERROR_STREAM(error);
+        throw std::runtime_error{error};
+    }
+    return true;
+}
+
+bool hasParamWarn(ros::NodeHandle *nh, const std::string &param) {
+    if (!nh->hasParam(param)) {
+        std::string error = "parameter " + param + " is not set. This needs to be specified as a rosparameter in the launchfile";
+        ROS_WARN_STREAM(error);
+        return false;
+    }
+    return true;
+}
+
+std::vector<Point> toVector(const nav_msgs::Path &path) {
+    std::vector<Point> pathVector(path.poses.size());
+    for (unsigned int i = 0; i < pathVector.size(); i++) {
+        pathVector.at(i) = toPoint(path.poses.at(i).pose.position);
+    }
+    return pathVector;
+}
+
+Point toPoint(const geometry_msgs::Point &p) {
+    return Point{p.x, p.y};
+}
+}  // namespace mpc
