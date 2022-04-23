ffa89a87f9fe19e1295d0a44727323c036393405
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2022-04-23 14:08:37 +0200

    using pitch and new format

--- src/RosMpc.cpp
+++ src/RosMpc.cpp
@@ -57,23 +57,18 @@ MPCReturn RosMpc::solve() {
         tfCar.transform.translation.y,
         getYaw(tfCar.transform.rotation),
         currentVel_,
-        0,
-        0};
-    Input input{
-        prevThrottle,
-        currentSteeringAngle_};
-    OptVariables optVars{state, input};
-    // mpc.model(optVars, input, 1.0 / loop_Hz_); // get predicted state after calculation is finished
+        currentSteeringAngle_,
+        prevThrottle};
 
     // solve mpc
-    const auto result = mpc.solve(optVars);
+    const auto result = mpc.solve(state, getPitch(tfCar.transform.rotation));
 
     // publish inputs
     std_msgs::Float64 msg;
-    msg.data = result.u0.throttle;
+    msg.data = result.mpcHorizon.at(1).x.throttle;
     throttlePub_.publish(msg);
-    prevThrottle = result.u0.throttle;
-    msg.data = result.u0.delta * steeringRatio_;
+    prevThrottle = msg.data;
+    msg.data = result.mpcHorizon.at(1).x.delta * steeringRatio_;
     steeringPub_.publish(msg);
 
     mpcPathPub_.publish(getPathMsg(result, mapFrame_, carFrame_));
@@ -82,8 +77,7 @@ MPCReturn RosMpc::solve() {
     }
 
     LOG_DEBUG("Time: %i [ms]", (int)result.computeTime);
-    LOG_DEBUG("carVel: %.2f, steering: %.2f [deg], throttle: %.2f", state.vel, result.u0.delta * 180.0 / M_PI, result.u0.throttle);
-    LOG_DEBUG("yaw error: %.2f", result.mpcHorizon.at(0).x.epsi * 180.0 / M_PI);
+    LOG_DEBUG("carVel: %.2f, steering: %.2f [deg], throttle: %.2f", state.vel, result.mpcHorizon.at(1).x.delta * 180.0 / M_PI, result.mpcHorizon.at(1).x.throttle);
     return result;
 }
 
