dbc43093ce8e6a47f510a8327838332e90f0cfd6
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2022-05-06 22:52:53 +0200

    using path tracking when far away form parking

--- include/mpc_local_planner/ParkingSys.h
+++ include/mpc_local_planner/ParkingSys.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <atomic>
+
 #include <geometry_msgs/Pose.h>
 
 #include "mpc_local_planner/AcadosPointStab.h"
 #include "mpc_local_planner/types.h"
+#include "mpc_local_planner/PathTrackingSys.h"
+#include "mpc_local_planner/utilities.h"
 
 namespace mpc {
 
@@ -11,24 +15,34 @@ class ParkingSys {
    public:
     ParkingSys() = default;
 
-    ParkingSys(const geometry_msgs::Pose &pose) : refPose_{pose}, init_{true} {
+    ParkingSys(const geometry_msgs::Pose &goal) : goal_{goal}, init_{true} {
     }
 
     MPCReturn solve(const State &state, double pitch);
 
     void setRefPose(const geometry_msgs::Pose &pose) {
-        refPose_ = pose;
+        goal_ = pose;
         init_ = true;
+        updatePath_ = true;
+    }
+
+    /// @brief get the current desired trajectory
+    /// @return track for the desired trajectory
+    std::vector<Point> getTrack() const {
+        return pathTrackingSys_.getTrack();
     }
 
     geometry_msgs::Pose getRefPose() const {
-        return refPose_;
+        return goal_;
     }
 
    private:
+    void createPathToGoal(const State &state);
     Acados::PointStab pointStabSolver_ = Acados::PointStab{State{0, 0, 0, 0, 0, 0}};
-    geometry_msgs::Pose refPose_;
+    PathTrackingSys pathTrackingSys_ = PathTrackingSys{getTestTrack()};
+    geometry_msgs::Pose goal_;
 
     bool init_ = false;
+    std::atomic<bool> updatePath_{false};
 };
 }  // namespace mpc
--- src/ParkingSys.cpp
+++ src/ParkingSys.cpp
@@ -1,18 +1,33 @@
 #include "mpc_local_planner/ParkingSys.h"
 
-#include "mpc_local_planner/utilities.h"
-
 namespace mpc {
 
 MPCReturn ParkingSys::solve(const State &state, double pitch) {
     if (!init_) {
         return MPCReturn{};
     }
-    Acados::PointStabParams params;
-    params.pitch = pitch;
-    params.pRef = Point{refPose_.position.x, refPose_.position.y};
-    params.psiRef = getYaw(refPose_.orientation);
-    return pointStabSolver_.solve(state, params);
+    double distSqrdToGoal = distSqrd(state.x - goal_.position.x, state.y - goal_.position.y);
+    if (distSqrdToGoal < 5 * 5) {
+        Acados::PointStabParams params;
+        params.pitch = pitch;
+        params.pRef = Point{goal_.position.x, goal_.position.y};
+        params.psiRef = getYaw(goal_.orientation);
+        return pointStabSolver_.solve(state, params);
+
+    } else {
+        if (updatePath_) {
+            createPathToGoal(state);
+            updatePath_ = false;
+        }
+        return pathTrackingSys_.solve(state, pitch);
+    }
 }
 
+void ParkingSys::createPathToGoal(const State &state) {
+    Point car{state.x, state.y};
+    Point goal{goal_.position.x, goal_.position.y};
+    BezierCurve bCurve{car, state.psi, goal, getYaw(goal_.orientation)};
+    pathTrackingSys_.setTrack(getPath(bCurve));
 }
+
+}  // namespace mpc
