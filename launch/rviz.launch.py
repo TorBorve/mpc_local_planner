49b76e52cb0aae76156887fa9220af4a0810edc1
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2022-12-08 21:08:26 +0100

    updated config files

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    rviz_file = os.path.join(get_package_share_directory("mpc_local_planner"), "rviz", "mpc_ros2.rviz")
+    rviz_file = os.path.join(get_package_share_directory("mpc_local_planner"), "config", "mpc_viz.rviz")
     print(f'rviz file: {rviz_file}')
     return LaunchDescription([
         launch_ros.actions.Node(
