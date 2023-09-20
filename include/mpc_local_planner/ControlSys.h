2d4b0c36122dda1e1cad87d2e7364afd71290a69
Tor Børve Rasmussen <rasmustor18@gmail.com>
2023-09-20 18:12:47 +0200

    removed parking mpc

--- include/mpc_local_planner/ControlSys.h
+++ include/mpc_local_planner/ControlSys.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "mpc_local_planner/ParkingSys.h"
 #include "mpc_local_planner/PathTrackingSys.h"
 #include "mpc_local_planner/types.h"
 #include "mpc_local_planner/utilities.h"
@@ -9,8 +8,8 @@ namespace mpc {
 
 class ControlSys {
    public:
-    ControlSys(double pathTrackingVel, double parkingVel)
-        : parkingSys_{parkingVel}, pathTrackingVel_{pathTrackingVel} {}
+    ControlSys(double pathTrackingVel)
+        : pathTrackingVel_{pathTrackingVel} {}
 
     /// @brief solve function
     /// @param[in] state the state of the car
@@ -25,29 +24,10 @@ class ControlSys {
     /// @return track for the desired trajectory
     std::vector<Point> getTrack() const;
 
-    void setMode(const Mode &mode) {
-        mode_ = mode;
-        parkingSys_.setMode(mode);
-    }
-
-    Mode getMode() const { return mode_; }
-    /// @brief set the reference pose for parking system.
-    /// @param[in] pose the pose we want to get to.
-    void setRefPose(const geometry_msgs::msg::Pose &pose) { parkingSys_.setRefPose(pose); }
-
-    /// @brief get the reference pose for the parking system.
-    geometry_msgs::msg::Pose getRefPose() const { return parkingSys_.getRefPose(); }
-
    private:
-    /// @brief the parking system class
-    ParkingSys parkingSys_;
-
     /// @brief the path tracking system class
     PathTrackingSys pathTrackingSys_ =
         PathTrackingSys{util::getTestTrack()};  // TODO remove use of getTestTrack
-
-    /// @brief mode of the control system.
-    Mode mode_ = Mode::Invalid;
 
     /// @brief the desired speed of the car when we are doing path tracking
     double pathTrackingVel_;
